d69623cc06006a92b688450c13d20a37c9b268f6
CodingSpiderFox <codingspiderfox@gmail.com>
2021-11-13 20:10:02 +0000

    fix: switch from mocked providers to full integration test against real providers

--- apps/bookinfo-api/src/app/auth/auth.service.spec.ts
+++ apps/bookinfo-api/src/app/auth/auth.service.spec.ts
@@ -1,31 +1,27 @@
-import { User, UsersService } from '@bookexample/users';
-import { JwtService } from '@nestjs/jwt';
+import { UsersModule } from '@bookexample/users';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
+import { LocalStrategy } from './local.strategy';
 
 describe('AuthService', () => {
   let service: AuthService;
-  const usersServiceMock = {
-    async findOne(email: string): Promise<User | undefined> {
-      return {
-        id: 1,
-        email: 'john',
-        password: 'changeme',
-      };
-    },
-  };
-
-  const jwtServiceMock = {};
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [AuthService, UsersService, JwtService],
-    })
-      .overrideProvider(UsersService)
-      .useValue(usersServiceMock)
-      .overrideProvider(JwtService)
-      .useValue(jwtServiceMock)
-      .compile();
+      imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.register({
+          secret: jwtConstants.secret,
+          signOptions: { expiresIn: '60s' },
+        }),
+      ],
+      providers: [AuthService, LocalStrategy, JwtStrategy],
+    }).compile();
 
     service = module.get(AuthService);
   });
